1510f47bfefd74df0e399c2b6eebd6ec128029f0
zqh <1327078020@qq.com>
2021-09-20 19:34:23 +0800

    master commit

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -3,5 +3,6 @@ public class HelloGit {
         System.out.println("hello,git");
         System.out.println("hello,world");
         System.out.println("hello,git2");
+        System.out.println("master test");
     }
 }
